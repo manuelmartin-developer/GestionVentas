a696839c00b718e59740c4c7ae793b280cbb2b26
Manuel Martín <hola@manuelmartin.name>
2021-05-20 12:25:52 +0200

    Final Project

---
+++ src/adtarea3/ADTarea3App.java
@@ -0,0 +1,44 @@
+/*
+ * ADTarea3App.java
+ */
+
+package adtarea3;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class ADTarea3App extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new ADTarea3View(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of ADTarea3App
+     */
+    public static ADTarea3App getApplication() {
+        return Application.getInstance(ADTarea3App.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(ADTarea3App.class, args);
+    }
+}
